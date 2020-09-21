99f88258df72f92e8a628bfcc2215969e3e329f1
NguyenQuang99 <quanghero7@gmail.com>
2020-09-21 16:30:14 +0700

    NextDayCalculator/

---
+++ NextDayCalculator/nextday.java
@@ -0,0 +1,92 @@
+package Calculator;
+import java.util.Arrays;
+
+public class NextDayCalculator {
+    public static String findNextDay(int inputDay, int inputMonth, int inputYear) {
+        int outputDay, outputMonth, outputYear;
+        int[] output = new int[3];
+        if (inputMonth == 4 || inputMonth == 6 || inputMonth == 9 || inputMonth == 11)
+            output = returnShortMonth(inputDay, inputMonth, inputYear);
+        else if (inputMonth == 2) {
+            output = calculateFebCase(inputDay,inputMonth,inputYear);
+        } else
+            output = returnLongMonth(inputDay, inputMonth, inputYear);
+        return display(output);
+    }
+
+    public static int[] returnLongMonth(int inputDay, int inputMonth, int inputYear) {
+        int[] output = new int[3];
+        if (inputDay != 31) {
+            return output = calculateNormalCase(inputDay, inputMonth, inputYear);
+        } else {
+            if (inputMonth == 12) {
+                int outputYear = inputYear + 1;
+                output[0] = 1;
+                output[1] = 1;
+                output[2] = outputYear;
+                return output;
+            } else {
+                output = calculateEndMonthCase(inputDay, inputMonth, inputYear);
+                return (output);
+            }
+        }
+    }
+
+    public static int[] returnShortMonth(int inputDay, int inputMonth, int inputYear) {
+        if (inputDay != 30) {
+            return calculateNormalCase(inputDay, inputMonth, inputYear);
+        } else {
+            return calculateEndMonthCase(inputDay, inputMonth, inputYear);
+        }
+    }
+
+    public static boolean isLeapYear(int inputYear) {
+        boolean isDivisibleBy4 = inputYear % 4 == 0;
+        boolean isLeapYear;
+        if (isDivisibleBy4) {
+            boolean isDivisibleBy100 = inputYear % 100 == 0;
+            if (isDivisibleBy100) {
+                boolean isDivisibleBy400 = inputYear % 400 == 0;
+                if (isDivisibleBy400) {
+                    return true;
+                } else {
+                    return false;
+                }
+            } else {
+                return true;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    public static int[] calculateEndMonthCase(int inputDay, int inputMonth, int inputYear) {
+        int[] output = {1, inputMonth + 1, inputYear};
+        return output;
+    }
+
+    public static int[] calculateNormalCase(int inputDay, int inputMonth, int inputYear) {
+        int[] output = {inputDay+1, inputMonth,inputYear};
+        return output;
+    }
+
+    public static int[] calculateFebCase(int inputDay, int inputMonth, int inputYear){
+        int[] output = new int[3];
+        if (isLeapYear(inputYear)) {
+            if (inputDay < 29) {
+                output = calculateNormalCase(inputDay, inputMonth, inputYear);
+            } else if (inputDay == 29)
+                output = calculateEndMonthCase(inputDay, inputMonth, inputYear);
+        } else {
+            if (inputDay < 28) {
+                output = calculateNormalCase(inputDay, inputMonth, inputYear);
+            } else if (inputDay == 28) {
+                output = calculateEndMonthCase(inputDay, inputMonth, inputYear);
+            }
+        }
+        return output;
+    }
+    public static String display(int[] output) {
+        return Arrays.toString(output);
+    }
+}
---
+++ NextDayCalculator/test.java
@@ -0,0 +1,68 @@
+package Calculator;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NextDayCalculatorTest {
+    @Test
+    void findNextDayDay1Month1() {
+        int thisDay = 1;
+        int thisMonth = 1;
+        int thisYear = 2018;
+        String expected = "[2, 1, 2018]";
+        String result = NextDayCalculator.findNextDay(thisDay,thisMonth,thisYear);
+        assertEquals(expected,result);
+    }
+
+    @Test
+    void findNextDayDay31Month1() {
+        int thisDay = 31;
+        int thisMonth = 1;
+        int thisYear = 2018;
+        String expected = "[1, 2, 2018]";
+        String result = NextDayCalculator.findNextDay(thisDay,thisMonth,thisYear);
+        assertEquals(expected,result);
+    }
+
+    @Test
+    void findNextDayDay30Month4() {
+        int thisDay = 30;
+        int thisMonth = 4;
+        int thisYear = 2018;
+        String expected = "[1, 5, 2018]";
+        String result = NextDayCalculator.findNextDay(thisDay,thisMonth,thisYear);
+        assertEquals(expected,result);
+    }
+
+    @Test
+    void findNextDayDay28Month2() {
+        int thisDay = 28;
+        int thisMonth = 2;
+        int thisYear = 2018;
+        String expected = "[1, 3, 2018]";
+        String result = NextDayCalculator.findNextDay(thisDay,thisMonth,thisYear);
+        assertEquals(expected,result);
+    }
+
+    @Test
+    void findNextDayDay29Month2() {
+        int thisDay = 29;
+        int thisMonth = 2;
+        int thisYear = 2020;
+        String expected = "[1, 3, 2020]";
+        String result = NextDayCalculator.findNextDay(thisDay,thisMonth,thisYear);
+        assertEquals(expected,result);
+    }
+
+    @Test
+    void findNextDayDay31Month12() {
+        int thisDay = 31;
+        int thisMonth = 12;
+        int thisYear = 2018;
+        String expected = "[1, 1, 2019]";
+        String result = NextDayCalculator.findNextDay(thisDay,thisMonth,thisYear);
+        assertEquals(expected,result);
+    }
+
+
+}
