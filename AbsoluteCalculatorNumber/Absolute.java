7ddf26cb6b8f032e426a95521edfd2e77a537f9a
NguyenQuang99 <quanghero7@gmail.com>
2020-09-21 09:45:02 +0700

    AbsoluteCalculatorNumber/

---
+++ AbsoluteCalculatorNumber/Absolute.java
@@ -0,0 +1,9 @@
+package Absolute_Number;
+
+public class AbsoluteCalculator {
+    public static int findAbsolute(int number) {
+        if(number < 0)
+            return -number;
+        return number;
+    }
+}
---
+++ AbsoluteCalculatorNumber/Test.java
@@ -0,0 +1,35 @@
+package Absolute_Number;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AbsoluteCalculatorTest {
+
+    @Test
+    void testFindAbsolute0() {
+        int number = 0;
+        int expected = 0;
+
+        int result = AbsoluteCalculator.findAbsolute(number);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void testFindAbsolute1() {
+        int number = 1;
+        int expected = 1;
+        int result = AbsoluteCalculator.findAbsolute(number);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void testFindAbsoluteNegative1() {
+        int number = -1;
+        int expected = 1;
+
+        int result = AbsoluteCalculator.findAbsolute(number);
+        assertEquals(expected, result);
+
+    }
+}
